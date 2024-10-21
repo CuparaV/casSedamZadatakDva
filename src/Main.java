17924f4a48dd9cf12363c2922546b373717bac6e
Vladimir Cupara <vladimir.cupara@nfinnova.com>
2024-10-21 16:16:10 +0200

    zadatak dva

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+/*
+Napraviti klasu Student sa 3 polja, ime, prezime i ocena.
+- Napraviti klasu koja sadrzi main metodu i u njoj napraviti minimum 4 studenta cije podatke treba uneti preko scanner-a: ime prezime i ocena.
+- Ocene treba da budu 5,6,7 i 9.
+- Kreirati dve liste.
+- Kada se preko scanner-a unesu podaci za studenta, taj student treba da se doda u listu.
+- Dodati sve studente u jednu listu, a nakon toga u drugu dodati samo one koji su polozili tj. koji imaju ocenu vecu od 5.
+- Potrebno je to uraditi u petlji gde ce unos podataka za studente biti omogucen sve dok se ne unese "quit". Quit se moze uneti na bilo kom koraku unosa.
+- Kada se unese "quit", program se prekida i ispisuje sve unete studente preko scanner-a, a nakon toga i one koji su polozili tj. koji imaju vecu ocenu od 5.
+ */
 public class Main {
     public static void main(String[] args) {
 
